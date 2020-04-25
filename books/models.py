74db6e930aa2c1bca4e2ebc48011094eab4b238a
adam <asoliyev13@gmail.com>
2020-04-25 00:08:23 -0400

    [Adam] Added book cover feature

--- books/models.py
+++ books/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 import uuid
 from django.contrib.auth import get_user_model
 
+
 class Book(models.Model):
     id = models.UUIDField(
         primary_key=True,
@@ -12,6 +13,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return self.title
